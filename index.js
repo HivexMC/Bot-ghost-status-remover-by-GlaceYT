98d2aa73cee96d4c8ff60785ca1a62a2e81bfe87
HivexMC <84274748+HivexMC@users.noreply.github.com>
2024-11-24 14:50:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["Atendiento dudas y preguntas", "Asistiendo a usuarios"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
